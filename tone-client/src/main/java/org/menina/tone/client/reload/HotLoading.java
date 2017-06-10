58627fb8c316b35035538c6dea3df461f9e91d94
MeninaChimp <zh179092007@gmail.com>
2017-06-10 23:25:21 +0800

    实现配置热加载

---
+++ tone-client/src/main/java/org/menina/tone/client/reload/HotLoading.java
@@ -0,0 +1,9 @@
+package org.menina.tone.client.reload;
+
+/**
+ * Created by Menina on 2017/6/10.
+ */
+public interface HotLoading {
+
+    void reload(String propertyName, String newValue);
+}
--- tone-client/src/main/java/org/menina/tone/client/reload/HotLoadingProcessor.java
+++ tone-client/src/main/java/org/menina/tone/client/reload/HotLoadingProcessor.java
@@ -1,14 +1,46 @@
 package org.menina.tone.client.reload;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
 /**
  * Created by Menina on 2017/6/10.
  */
-public class HotLoadingProcessor {
+@Slf4j
+@Component
+public class HotLoadingProcessor implements HotLoading, ApplicationContextAware{
 
-    //// 责任链实现的Filter
+    private ApplicationContext act;
 
+    @Override
+    public void reload(String propertyName, String newValue) {
+        Set<String> beanNames = ConfiguredBeansHolder.getConfiguredBeans(propertyName);
+        for(String beanName : beanNames){
+            Object bean = act.getBean(beanName);
+            String oldValue;
+            if(bean != null){
+                try{
+                    BeanWrapper beanWrapper = new BeanWrapperImpl(bean);
+                    oldValue = beanWrapper.getPropertyValue(propertyName).toString();
+                    beanWrapper.setPropertyValue(propertyName, newValue);
+                    log.info(String.format("update property: %s success, oldValue: %s, new Value: %s"), propertyName, oldValue, newValue);
+                }catch (Exception e){
+                    log.error(String.format("Failed to hot-reload property: %s value to newValue: %s, %s", propertyName, newValue, e.getMessage()), e);
+                }
+            }
+        }
+    }
 
-    public static void reload(String propertyName, String newValue){
-
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.act = applicationContext;
     }
 }
