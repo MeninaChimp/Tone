fd8134bac20d8c10658aca44ddb58b32e54be9aa
MeninaChimp <zh179092007@gmail.com>
2017-06-11 00:11:55 +0800

    完成zk配置资源获取

--- tone-client/src/main/java/org/menina/tone/client/source/zookeeper/ZookeeperListener.java
+++ tone-client/src/main/java/org/menina/tone/client/source/zookeeper/ZookeeperListener.java
@@ -1,15 +1,20 @@
 package org.menina.tone.client.source.zookeeper;
 
+import lombok.Data;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.framework.api.CuratorListener;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.menina.tone.client.source.PropertyChangeListener;
 
 /**
  * Created by Menina on 2017/6/10.
  */
+@Data
 public class ZookeeperListener implements CuratorListener{
+
+    private PropertyChangeListener propertyChangeListener = new ZookeeperPropertyChangeListener();
 
     @Override
     public void eventReceived(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
@@ -17,9 +22,8 @@ public class ZookeeperListener implements CuratorListener{
         if (watchedEvent != null) {
             if (watchedEvent.getState() == Watcher.Event.KeeperState.SyncConnected) {
                 switch (watchedEvent.getType()) {
-                    case NodeChildrenChanged:
-                        break;
                     case NodeDataChanged:
+                        propertyChangeListener.reloadProperty(watchedEvent.getPath());
                         break;
                     default:
                         break;
--- tone-client/src/main/java/org/menina/tone/client/source/zookeeper/ZookeeperResourceLoader.java
+++ tone-client/src/main/java/org/menina/tone/client/source/zookeeper/ZookeeperResourceLoader.java
@@ -1,34 +1,63 @@
 package org.menina.tone.client.source.zookeeper;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Maps;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.GetChildrenBuilder;
+import org.apache.curator.framework.api.GetDataBuilder;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.utils.ZKPaths;
 import org.menina.tone.client.source.ResourceLoaderAdapter;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Menina on 2017/6/10.
  */
-public class ZookeeperResourceLoader extends ResourceLoaderAdapter{
+public class ZookeeperResourceLoader extends ResourceLoaderAdapter {
 
     private volatile static CuratorFramework client;
 
     @Override
     public Map<String, String> loads(String path) {
-        return null;
+        GetChildrenBuilder childrenBuilder = this.instance().getChildren();
+        Map<String, String> propertiesMap = Maps.newHashMap();
+        try {
+            List<String> nodes = childrenBuilder.forPath(path);
+            if (nodes != null) {
+                for (String node : nodes) {
+                    Map.Entry<String, String> data = this.loadProperty(ZKPaths.makePath(path, node));
+                    if (data != null) {
+                        propertiesMap.put(data.getKey(), data.getValue());
+                    }
+                }
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException(String.format("Failed to load properties by given path, %s", t.getMessage()), t);
+        }
+
+        return propertiesMap;
     }
 
     @Override
     public Map.Entry<String, String> loadProperty(String nodePath) {
-        return null;
+        String propertyName = ZKPaths.getNodeFromPath(nodePath);
+        GetDataBuilder data = this.instance().getData();
+        try {
+            String propertyValue = new String(data.watched().forPath(nodePath), Charsets.UTF_8);
+            return Maps.immutableEntry(propertyName, propertyValue);
+        }catch (Throwable t) {
+            throw new RuntimeException(String.format("Failed to load property by given nodePath, %s", t.getMessage()), t);
+        }
     }
 
 
-    private CuratorFramework instance(){
-        if(null == client){
-            synchronized (ZookeeperResourceLoader.class){
+    private CuratorFramework instance() {
+        if (null == client) {
+            synchronized (ZookeeperResourceLoader.class) {
                 CuratorFramework client = CuratorFrameworkFactory.newClient(
                         this.getUrl(),
                         2000,
@@ -39,10 +68,7 @@ public class ZookeeperResourceLoader extends ResourceLoaderAdapter{
             }
         }
 
+        client.getCuratorListenable().addListener(new ZookeeperListener());
         return client;
-    }
-
-    private String makePath(){
-        return ZKPaths.makePath(this.getVersion(), this.getPath());
     }
 }
