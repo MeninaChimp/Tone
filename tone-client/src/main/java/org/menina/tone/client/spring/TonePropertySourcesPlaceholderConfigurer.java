1d09836354c3e31884b6a8fc0f99be42c4ffeea2
MeninaChimp <zh179092007@gmail.com>
2017-06-11 01:50:51 +0800

    加载配置数据到Spring PropertySources

--- tone-client/src/main/java/org/menina/tone/client/spring/TonePropertySourcesPlaceholderConfigurer.java
+++ tone-client/src/main/java/org/menina/tone/client/spring/TonePropertySourcesPlaceholderConfigurer.java
@@ -1,7 +1,7 @@
 package org.menina.tone.client.spring;
 
+import org.menina.tone.client.properties.TonePropertyConfiguration;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.Ordered;
@@ -9,48 +9,31 @@ import org.springframework.core.PriorityOrdered;
 import org.springframework.core.env.*;
 import org.springframework.util.Assert;
 
-import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by Menina on 2017/6/10.
  */
 public class TonePropertySourcesPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer implements PriorityOrdered{
 
+    private TonePropertyConfiguration tonePropertyConfiguration;
+
     private MutablePropertySources propertySources;
 
     private PropertySources appliedPropertySources;
 
-    private Environment environment;
+    private PropertySourceLoader propertySourceLoader;
+
+    private boolean allowOverride;
 
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        if (this.propertySources == null) {
+        if(this.propertySources == null){
             this.propertySources = new MutablePropertySources();
-            if (this.environment != null) {
-                this.propertySources.addLast(
-                        new PropertySource<Environment>(ENVIRONMENT_PROPERTIES_PROPERTY_SOURCE_NAME, this.environment) {
-                            @Override
-                            public String getProperty(String key) {
-                                return this.source.getProperty(key);
-                            }
-                        }
-                );
-            }
-            try {
-                PropertySource<?> localPropertySource =
-                        new PropertiesPropertySource(LOCAL_PROPERTIES_PROPERTY_SOURCE_NAME, mergeProperties());
-                if (this.localOverride) {
-                    this.propertySources.addFirst(localPropertySource);
-                }
-                else {
-                    this.propertySources.addLast(localPropertySource);
-                }
-            }
-            catch (IOException ex) {
-                throw new BeanInitializationException("Could not load properties", ex);
-            }
         }
 
+        Map<String, String> propertySource = this.propertySourceLoader.loadResource(this.tonePropertyConfiguration.makePaths().toArray(new String[]{}));
+        this.propertySources.addFirst(new TonePropertySource(TonePropertySource.name, propertySource));
         processProperties(beanFactory, new PropertySourcesPropertyResolver(this.propertySources));
         this.appliedPropertySources = this.propertySources;
     }
@@ -63,5 +46,17 @@ public class TonePropertySourcesPlaceholderConfigurer extends PropertySourcesPla
     public PropertySources getAppliedPropertySources() throws IllegalStateException {
         Assert.state(this.appliedPropertySources != null, "PropertySources have not get been applied");
         return this.appliedPropertySources;
+    }
+
+    public void setTonePropertyConfiguration(TonePropertyConfiguration tonePropertyConfiguration) {
+        this.tonePropertyConfiguration = tonePropertyConfiguration;
+    }
+
+    public void setPropertySourceLoader(PropertySourceLoader propertySourceLoader) {
+        this.propertySourceLoader = propertySourceLoader;
+    }
+
+    public void setAllowOverride(boolean allowOverride) {
+        this.allowOverride = allowOverride;
     }
 }
