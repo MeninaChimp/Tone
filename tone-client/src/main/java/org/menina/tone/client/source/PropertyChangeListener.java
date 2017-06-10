ed5c7a957acf4e8026ec5607c95ed7245f1a4b34
MeninaChimp <zh179092007@gmail.com>
2017-06-10 13:57:42 +0800

    Initialize load property interface and property change listener interface

---
+++ tone-client/src/main/java/org/menina/tone/client/source/PropertyChangeListener.java
@@ -0,0 +1,8 @@
+package org.menina.tone.client.source;
+
+/**
+ * Created by Menina on 2017/6/10.
+ */
+public interface PropertyChangeListener<K, V>
+{
+}
--- tone-client/src/main/java/org/menina/tone/client/source/ResouceLoader.java
+++ tone-client/src/main/java/org/menina/tone/client/source/ResouceLoader.java
@@ -1,11 +1,14 @@
 package org.menina.tone.client.source;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * Created by Menina on 2017/6/10.
  */
 public interface ResouceLoader<K, V> {
 
-    Map<K, V> load();
+    Map<String, String> load();
+
+    Entry<String, String> loadKey(String key);
 }
