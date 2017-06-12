7a4c16b7da91be106d7b941bfc93731af6d4e663
MeninaChimp <zh179092007@gmail.com>
2017-06-12 22:57:52 +0800

    调整应用自动配置项目

--- tone-client/src/main/java/org/menina/tone/client/spring/ToneAutoConfiguration.java
+++ tone-client/src/main/java/org/menina/tone/client/spring/ToneAutoConfiguration.java
@@ -1,55 +1,110 @@
 package org.menina.tone.client.spring;
 
+import com.google.common.reflect.ClassPath;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.api.CuratorEvent;
+import org.apache.curator.framework.api.CuratorListener;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.menina.tone.client.listener.ToneListener;
 import org.menina.tone.client.listener.Listener;
 import org.menina.tone.client.properties.TonePropertyConfiguration;
 import org.menina.tone.client.source.PropertyChangeListener;
 import org.menina.tone.client.source.ResourceLoader;
+import org.menina.tone.client.source.zookeeper.ZookeeperPropertyChangeListener;
 import org.menina.tone.client.source.zookeeper.ZookeeperResourceLoader;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 
 /**
  * Created by Menina on 2017/6/10.
  */
+@Slf4j
 @Configuration
+@PropertySource(value = "classpath:tone.properties", ignoreResourceNotFound = true)
 public class ToneAutoConfiguration {
 
-    @Autowired(required = false)
-    private PropertyChangeListener propertyChangeListener;
-
-    @Autowired(required = false)
-    private ResourceLoader resourceLoader;
-
-    @Autowired
-    private TonePropertyConfiguration tonePropertyConfiguration;
-
     @Bean
-    public Listener defaultListener(){
+    public Listener defaultListener() {
         return new ToneListener();
     }
 
     @Bean
-    private PropertySourceLoader springPropertySourceLoader(){
-        PropertySourceLoader propertySourceLoader = new SpringPropertySourceLoader();
-        if(null == this.resourceLoader){
-            this.resourceLoader = new ZookeeperResourceLoader();
-            this.resourceLoader.setUrl(tonePropertyConfiguration.getAddress());
-        }
-
-        propertySourceLoader.setResourceLoader(this.resourceLoader);
-        return propertySourceLoader;
-    }
-
-    @Bean
-    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         TonePropertySourcesPlaceholderConfigurer tonePropertySourcesPlaceholderConfigurer = new TonePropertySourcesPlaceholderConfigurer();
         tonePropertySourcesPlaceholderConfigurer.setAllowOverride(false);
-        tonePropertySourcesPlaceholderConfigurer.setPropertySourceLoader(this.springPropertySourceLoader());
-        tonePropertySourcesPlaceholderConfigurer.setTonePropertyConfiguration(this.tonePropertyConfiguration);
+        tonePropertySourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+        tonePropertySourcesPlaceholderConfigurer.setPropertySourceLoader(springPropertySourceLoader());
+        tonePropertySourcesPlaceholderConfigurer.setTonePropertyConfiguration(new TonePropertyConfiguration());
         return tonePropertySourcesPlaceholderConfigurer;
+    }
+
+    @Conditional(InterceptEnvironmentProperty.class)
+    @Bean
+    public TonePropertyConfiguration interceptEnvironmentProperty() {
+        return new TonePropertyConfiguration();
+    }
+
+    private static PropertySourceLoader springPropertySourceLoader() {
+        if (TonePropertyConfiguration.getResourceLoader() != null) {
+            Object userDefinedResourceLoader = null;
+            PropertySourceLoader propertySourceLoader;
+            try {
+                Class<?> clz;
+                try {
+                    clz = Class.forName(TonePropertyConfiguration.getResourceLoader());
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(String.format("The given user-defined resourceLoader path not found, %s", e.getMessage()), e);
+                }
+
+                userDefinedResourceLoader = clz.newInstance();
+                if (!(userDefinedResourceLoader instanceof ResourceLoader)) {
+                    throw new RuntimeException("The user-defined resourceLoader must implement \"ResourceLoader\" interface");
+                }
+            } catch (InstantiationException e) {
+                log.error(String.format("Failed to create resourceLoader instance by given implement, %s"), e.getMessage(), e);
+            } catch (IllegalAccessException e) {
+                log.error(e.getMessage(), e);
+            }
+
+            propertySourceLoader = new SpringPropertySourceLoader();
+            propertySourceLoader.setResourceLoader((ResourceLoader) userDefinedResourceLoader);
+            return propertySourceLoader;
+        }
+
+        return initDefaultPropertySourceLoader();
+    }
+
+    private static PropertySourceLoader initDefaultPropertySourceLoader() {
+        PropertySourceLoader propertySourceLoader = new SpringPropertySourceLoader();
+        ResourceLoader<CuratorListener> resourceLoader = new ZookeeperResourceLoader();
+        final PropertyChangeListener propertyChangeListener = new ZookeeperPropertyChangeListener();
+        resourceLoader.setUrl(TonePropertyConfiguration.getAddress());
+        resourceLoader.setResourceContainerListener(new CuratorListener() {
+            @Override
+            public void eventReceived(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
+                final WatchedEvent watchedEvent = curatorEvent.getWatchedEvent();
+                if (watchedEvent != null) {
+                    if (watchedEvent.getState() == Watcher.Event.KeeperState.SyncConnected) {
+                        switch (watchedEvent.getType()) {
+                            case NodeDataChanged:
+                                propertyChangeListener.reloadProperty(watchedEvent.getPath());
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        });
+
+        propertyChangeListener.setResourceLoader(resourceLoader);
+        propertySourceLoader.setResourceLoader(resourceLoader);
+        return propertySourceLoader;
     }
 }
